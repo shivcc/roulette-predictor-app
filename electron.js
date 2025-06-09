656867315f9b302318d7a99dd8770348bdef8718
Shivcc <167299515+shivcc@users.noreply.github.com>
2025-06-10 02:47:22 +0300

    Initial Commit

---
+++ electron.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  const startUrl = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
+
+  win.loadURL(startUrl);
+
+  if (isDev) {
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
